9d054666c82329ea78d6142f1e9a461a6f158996
Jeffrey Sheehan <jl.sheehan@aims.gov.au>
2019-08-05 09:54:48 +1000

    Changes to documentation

--- R/aimsdataplatform.R
+++ R/aimsdataplatform.R
@@ -3,23 +3,13 @@ require("jsonlite")
 
 defaultBaseEndPoint <- "https://dbh6gco67g.execute-api.ap-southeast-2.amazonaws.com/test/data-by-doi"
 
-# Get values for a filter and return a vector
-# getFilterValues <- function(doi, filterName, baseEndPoint=defaultBaseEndPoint) {
-#   endPoint <- paste(baseEndPoint, doi, "filters", filterName, sep='/')
-#   filterValues <- GET(endPoint)
-#   jsonResponse <- content(filterValues, "text", encoding = "UTF-8")
-#   filterValuesVector <- fromJSON(jsonResponse , simplifyVector = TRUE)
-#   print(filterValuesVector)
-#   return(filterValuesVector)
-# }
-
 # Get some data and return a data frame
 getData <- function(doi, filters=NULL, baseEndPoint=defaultBaseEndPoint) {
   endPoint <- paste(baseEndPoint, doi, "data", sep="/")
   dataRequest <- GET(endPoint, query = filters)
   jsonResponse <- content(dataRequest, "text", encoding = "UTF-8")
   dataFrame <- fromJSON(jsonResponse, simplifyDataFrame = TRUE)
-  print(dataFrame)
+  #print(dataFrame)
   return(dataFrame)
 }
 
@@ -30,7 +20,7 @@ dataplatformclient <- function(baseEndPoint = defaultBaseEndPoint) {
   return(c)
 }
 
-dataplatformclient.getData <- function(object, doi, filters=NULL) {
+dataplatform.getData <- function(object, doi, filters=NULL) {
   return(getData(doi, filters=filters, baseEndPoint=object$baseEndPoint))
 }
 
