87da08ed25dc22db6135e1a402716f48abd137b5
Jeffrey Sheehan <jl.sheehan@aims.gov.au>
2019-08-12 13:45:12 +1000

    Add list of sites

--- docs/sites.js
+++ docs/sites.js
@@ -1,20 +1,10 @@
-AIMS DataPlatform R Client
-==========================
-
-The list of sites is:
-
-<div id="sites"></div>
-
-<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-<script>
+// load sites
 
 $.get("https://b5ms5dkmia.execute-api.ap-southeast-2.amazonaws.com/prod/data-by-doi/10.25845/5c09bf93f315d/sites", populate)
 
-
 function populate(data) {
+
   var sites = JSON.parse(data);
   sites.forEach(site => $("sites").append(site));
   console.log(sites);
 }
-
-</script>
