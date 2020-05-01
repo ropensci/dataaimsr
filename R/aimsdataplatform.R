c5ee9f608e300e466271e8bf4fd7678c3a8c2fb7
Diego Barneche <diego.barneche@ymail.com>
2020-05-01 11:56:40 +0800

    multiple changes to existing code to abide to coding style and practices

--- R/aimsdataplatform.R
+++ R/aimsdataplatform.R
@@ -1,107 +1,106 @@
-library(httr)
-library(jsonlite)
-
-defaultBaseEndPoint <- "https://api.aims.gov.au/data/v1.0"
-#defaultBaseEndPoint <- "https://6aq0l8l806.execute-api.ap-southeast-2.amazonaws.com/test/v1.0"
-
-defaultDateFormat <- "%Y-%m-%dT%H:%M:%OS"
-
-aimsWeather <- "10.25845/5c09bf93f315d"
-aimsTemperatureLoggers <- "10.25845/5b4eb0f9bb848"
-
-# Get some data and return a data frame
-getPageData <- function(doi, filters=NULL, baseEndPoint=defaultBaseEndPoint, apiKey=NULL) {
-  endPoint <- paste(baseEndPoint, doi, "data", sep="/")
-  dataRequest <- GET(endPoint, add_headers("X-Api-Key"=findApiKey(apiKey)), query=filters)
-  if (http_error(dataRequest)) {
-    warning(paste("Error", http_status(dataRequest), content(dataRequest)))
-  } else {
-    jsonResponse <- content(dataRequest, "text", encoding = "UTF-8")
-    results <- fromJSON(jsonResponse, simplifyDataFrame = TRUE)
-    results <- updateFormat(results)
-    print(paste("Cite this data as:", results$citation))
-    return(results)
-  }
+request_warning <- function(dt_req) {
+  warning(paste("Error", httr::http_status(dt_req),
+                httr::content(dt_req)))
 }
 
-# Get some data and return a data frame
-getNextPageData <- function(url, apiKey=NULL) {
-  parsed_url <- parse_url(url)
-  # Unfortunately need to remove plus sign if not decoded properly
-  parsed_url$query$cursor <- gsub("\\+", " ", parsed_url$query$cursor)
-  dataRequest <- GET(parsed_url, add_headers("X-Api-Key"=findApiKey(apiKey)))
-  if (http_error(dataRequest)) {
-    warning(paste("Error", http_status(dataRequest), content(dataRequest)))
+json_results  <-  function(dt_req) {
+    json_resp <- httr::content(dt_req, "text", encoding = "UTF-8")
+    jsonlite::fromJSON(json_resp, simplifyDataFrame = TRUE)
+}
+
+request_process <- function(dt_req, next_page = FALSE) {
+  if (httr::http_error(dt_req)) {
+    request_warning(dt_req)
   } else {
-    jsonResponse <- content(dataRequest, "text", encoding = "UTF-8")
-    results <- fromJSON(jsonResponse, simplifyDataFrame = TRUE)
-    if (nrow(results$results)==0) {
+    results <- json_results(dt_req)
+    if (next_page && nrow(results$results) == 0) {
       warning("No more data")
     } else {
-      results <- updateFormat(results)
-      return(results)
-    }
-  }
-}
-
-getData <- function(doi, filters=NULL, baseEndPoint=defaultBaseEndPoint, apiKey=NULL) {
-  results = getPageData(doi, filters=filters, baseEndPoint=baseEndPoint, apiKey=apiKey)
-  nextUrl <- results$links$nextPage
-  moreData <<- TRUE
-  while(moreData) {
-    tryCatch({
-      nextResults <- getNextPageData(nextUrl, apiKey=apiKey)
-      newDataFrame <- rbind(results$dataFrame, nextResults$dataFrame)
-      results$dataFrame <- newDataFrame
-      if ("links" %in% names(nextResults) && "nextPage" %in% names(nextResults$links)) {
-        nextUrl <- nextResults$links$nextPage
-      } else {
-        moreData <<- FALSE
+      results <- format_update(results)
+      if (!next_page) {
+        print(paste("Cite this data as:", results$citation))
       }
-    }, warning = function(w) {
-      moreData <<- FALSE
-    }, error = function(e) {
-      #print(paste("getAllError", e))
-      moreData <<- FALSE
+      results
+    }
+  }
+}
+
+# Get some data and return a data frame
+page_data_get <- function(doi, filters = NULL, api_key = NULL) {
+  base_end_pt <- getOption("aimsdataplatform.base_end_point")
+  end_pt <- paste(base_end_pt, doi, "data", sep = "/")
+  dt_req <- httr::GET(end_pt,
+                      httr::add_headers("X-Api-Key" = api_key_find(api_key)),
+                      query = filters)
+  request_process(dt_req)
+}
+
+# Get some data and return a data frame
+next_page_data_get <- function(url, api_key = NULL) {
+  parsed_url <- httr::parse_url(url)
+  # Unfortunately need to remove plus sign if not decoded properly
+  parsed_url$query$cursor <- gsub("\\+", " ", parsed_url$query$cursor)
+  dt_req <- httr::GET(parsed_url,
+                      httr::add_headers("X-Api-Key" = api_key_find(api_key)))
+  request_process(dt_req, next_page = TRUE)
+}
+
+#' @ ... api_key to page_data_get and next_page_data_get
+data_get <- function(doi, filters = NULL, ...) {
+  results <- page_data_get(doi, filters = filters, ...)
+  next_url <- results$links[["next"]]
+  more_data <- TRUE
+  while (more_data) {
+    tryCatch({
+      next_res <- next_page_data_get(next_url, ...)
+      new_df <- rbind(results$data, next_res$data)
+      results$data <- new_df
+      if ("links" %in% names(next_res) &&
+          "next_page" %in% names(next_res$links)) {
+        next_url <- next_res$links[["next"]]
+      } else {
+        more_data <- FALSE
+      }
+    }, warning = function() {
+      more_data <<- FALSE
+    }, error = function() {
+      more_data <<- FALSE
     })
-    print(paste("Result count:", nrow(results$dataFrame)))
+    print(paste("Result count:", nrow(results$data)))
   }
-  return(results)
+  results
 }
 
-getFilterValues <- function(doi, filterName, baseEndPoint = defaultBaseEndPoint) {
-  endPoint <- paste(baseEndPoint, doi, filterName, sep="/")
-  dataRequest <- GET(endPoint)
-  if (http_error(dataRequest)) {
-    warning(paste("Error", http_status(dataRequest), content(dataRequest)))
+filter_values_get <- function(doi, filter_name) {
+  base_end_pt <- getOption("aimsdataplatform.base_end_point")
+  end_pt <- paste(base_end_pt, doi, filter_name, sep = "/")
+  dt_req <- httr::GET(end_pt)
+  if (httr::http_error(dt_req)) {
+    request_warning(dt_req)
   } else {
-    jsonResponse <- content(dataRequest, "text", encoding = "UTF-8")
-    results <- fromJSON(jsonResponse, simplifyDataFrame = TRUE)
-    return(results)
+    json_results(dt_req)
   }
 }
 
-updateFormat <- function(results) {
-  if ("links" %in% names(results) && "next" %in% names(results$links)) {
-    results$links$nextPage <- results$links$'next'
-    results$links$'next' <- NULL
-  }
-  results$results$time <- as.POSIXct(strptime(results$results$time, format=defaultDateFormat, tz="UTC"))
-  names(results)[names(results) == "results"] <- "dataFrame"
-  return(results)
+format_update <- function(results) {
+  date_format <- getOption("aimsdataplatform.date_format")
+  results$results$time <- as.POSIXct(strptime(results$results$time,
+                                              format = date_format,
+                                              tz = "UTC"))
+  names(results)[names(results) == "results"] <- "data"
+  results
 }
 
-findApiKey <- function(apiKey) {
-  if (is.null(apiKey)) {
-    rEnvironApiKey <- Sys.getenv("AIMS_DATAPLATFORM_API_KEY")
-    if (is.null(rEnvironApiKey)) {
-      warning("No API Key could be found, please see https://aims.github.io/data-platform/key-request")
+api_key_find <- function(api_key) {
+  if (is.null(api_key)) {
+    r_env_api_key <- Sys.getenv("AIMS_DATAPLATFORM_API_KEY")
+    if (is.null(r_env_api_key)) {
+      warning("No API Key could be found, please see",
+              "https://aims.github.io/data-platform/key-request")
     } else {
-      return(rEnvironApiKey)
+      r_env_api_key
     }
   } else {
-    return(apiKey)
+    api_key
   }
 }
-
-# https://b5ms5dkmia.execute-api.ap-southeast-2.amazonaws.com/prod/data-by-doi/10.25845/5c09bf93f315d/data?site-name=Davies Reef&size=10
